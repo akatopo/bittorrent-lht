c8ae8f173a7685e9b3574e4bf4597b6de01a2771
Alex Katopodis <akatopo@users.noreply.github.com>
2025-08-27 02:18:43 +0300

    add basic http API to lht in-memory table

--- lht-in-memory-table.js
+++ lht-in-memory-table.js
@@ -1,57 +1,114 @@
 import { EventEmitter } from 'events'
 import Debug from 'debug'
 import { Lht } from './lht.js'
-import { lhtAnnounce } from './announce.js'
+import { lhtAnnounce, checkInfoHash } from './announce.js'
+import { createMagnetLink } from './magnet.js'
+import http from 'http'
+import Router from 'find-my-way'
 
 const debug = Debug('bittorrent-lht:lht-in-memory-table')
 const twentyMinutes = 20 * 60 * 1000
+
+const defaults = {
+  interfaceForMembership: undefined,
+  httpServerHost: 'localhost',
+  httpServerPort: 0
+}
 
 function getKey (infoHash, peer) {
   return JSON.stringify([infoHash, peer])
 }
 
+function createHttpServer (lhtInMemoryTable) {
+  const router = Router()
+
+  router.on('GET', '/lht', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.writeHead(200)
+    res.end(JSON.stringify(lhtInMemoryTable.infoHashPeerTable))
+  })
+
+  router.on('GET', '/lht/:infoHash', (req, res, params) => {
+    const { infoHash } = params
+    res.setHeader('Content-Type', 'application/json')
+    const notFound = () => {
+      res.writeHead(404)
+      res.end(JSON.stringify({ error: 'Resource not found' }))
+    }
+    if (!checkInfoHash(infoHash)) {
+      return notFound()
+    }
+    debug(`query ${infoHash}`)
+    const peers = lhtInMemoryTable.peersFromInfoHash(infoHash)
+    if (!peers.length) {
+      return notFound()
+    }
+
+    res.writeHead(200)
+    res.end(JSON.stringify(peers))
+  })
+
+  router.on('GET', '/magnet/:infoHash', (req, res, params) => {
+    const { infoHash } = params
+    res.setHeader('Content-Type', 'application/json')
+    const notFound = () => {
+      res.writeHead(404)
+      res.end(JSON.stringify({ error: 'Resource not found' }))
+    }
+    if (!checkInfoHash(infoHash)) {
+      return notFound()
+    }
+    debug(`query ${infoHash}`)
+    const peers = lhtInMemoryTable.peersFromInfoHash(infoHash)
+    if (!peers.length) {
+      return notFound()
+    }
+
+    res.writeHead(200)
+    res.end(JSON.stringify(createMagnetLink(infoHash, peers)))
+  })
+
+  const httpServer = http.createServer((req, res) => router.lookup(req, res))
+
+  return { httpServer, router }
+}
+
 export class LhtInMemoryTable extends EventEmitter {
-  constructor (interfaceForMembership) {
+  constructor ({
+    interfaceForMembership = defaults.interfaceForMembership,
+    httpServerHost = defaults.httpServerHost,
+    httpServerPort = defaults.httpServerPort
+  } = { ...defaults }) {
     super()
     this.lht = new Lht(interfaceForMembership)
+
     this.infoHashPeerMap = new Map()
     this.infoHashAndPeerTimeoutMap = new Map()
 
+    const { httpServer } = createHttpServer(this)
+    this.httpServer = httpServer
+    httpServer.on('listening', () => {
+      const { port } = httpServer.address()
+      debug(`Server is listening on http://${httpServerHost}:${port}`)
+    })
+    httpServer.listen(httpServerPort, httpServerHost)
+
     this.lht.on('peer', (...args) => {
-      const { infoHashPeerMap, infoHashAndPeerTimeoutMap } = this
       const [peer, infoHash] = args
 
-      const peersForHash = infoHashPeerMap.get(infoHash) ?? new Set()
-      peersForHash.add(peer)
-      infoHashPeerMap.set(infoHash, peersForHash)
-      const key = getKey(infoHash, peer)
-      const timeout = infoHashAndPeerTimeoutMap.get(key)
-      if (timeout) {
-        clearTimeout(timeout)
-      }
-      infoHashAndPeerTimeoutMap.set(key, setTimeout(() => {
-        debug(`removing ${key} from timeout map and LHT`)
-
-        infoHashAndPeerTimeoutMap.delete(key)
-        const peers = infoHashPeerMap.get(infoHash)
-        peers?.delete(peer)
-        if (peers?.size === 0) {
-          infoHashPeerMap.delete(infoHash)
-        }
-      }, twentyMinutes))
+      this.addPeer(peer, infoHash)
       this.emit('peer', peer, infoHash)
     })
 
     this.lht.on('lht', (...args) => {
-      const { infoHashPeerMap } = this
       const [infoHash] = args
-      const peers = infoHashPeerMap.get(infoHash) ?? new Set()
-      if (peers.size) {
-        const peer = [...peers.values()][0]
-        const announce = lhtAnnounce(infoHash, this.lht.cookie, peer)
+      const peers = this.peersFromInfoHash(infoHash)
+      if (peers.length) {
+        // const peer = peers[0]
+        const announce = lhtAnnounce(infoHash, this.lht.cookie, peers)
         this.lht.send(announce)
         this.emit('lht', announce)
-        debug(`LHT announce for ${infoHash} from ${peer}`)
+        debug(`LHT announce for ${infoHash}: ${peers}`)
       }
     })
   }
@@ -59,6 +116,36 @@ export class LhtInMemoryTable extends EventEmitter {
   get infoHashPeerTable () {
     const { infoHashPeerMap } = this
     return [...infoHashPeerMap.entries()].map(([infoHash, peers]) => [infoHash, [...peers.values()]])
+  }
+
+  peersFromInfoHash (infoHash) {
+    const { infoHashPeerMap } = this
+    const peers = infoHashPeerMap.get(infoHash) ?? new Set()
+
+    return [...peers.values()]
+  }
+
+  addPeer (peer, infoHash) {
+    const { infoHashPeerMap, infoHashAndPeerTimeoutMap } = this
+
+    const peersForHash = infoHashPeerMap.get(infoHash) ?? new Set()
+    peersForHash.add(peer)
+    infoHashPeerMap.set(infoHash, peersForHash)
+    const key = getKey(infoHash, peer)
+    const timeout = infoHashAndPeerTimeoutMap.get(key)
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    infoHashAndPeerTimeoutMap.set(key, setTimeout(() => {
+      debug(`removing ${key} from timeout map and LHT`)
+
+      infoHashAndPeerTimeoutMap.delete(key)
+      const peers = infoHashPeerMap.get(infoHash)
+      peers?.delete(peer)
+      if (peers?.size === 0) {
+        infoHashPeerMap.delete(infoHash)
+      }
+    }, twentyMinutes))
   }
 
   start () {
@@ -70,6 +157,7 @@ export class LhtInMemoryTable extends EventEmitter {
     infoHashAndPeerTimeoutMap.forEach(timeout => {
       clearTimeout(timeout)
     })
+    this.httpServer.close(() => this.httpServer.closeAllConnections())
     return this.lht.destroy(cb)
   }
 }
--- lht-server.js
+++ lht-server.js
@@ -5,10 +5,16 @@ import { program } from 'commander'
 
 program
   .option('-i, --network-interface <interface>', 'The network interface whose IPV4 address will be used to join the LSD multicast group')
+  .option('-h, --http-host <host>', 'The host that the http server will listen to')
+  .option('-p, --http-port <port>', 'The port that the http server will listen to')
   .parse()
 
 const options = program.opts()
 
-const server = new Server(options.networkInterface ?? undefined)
+const server = new Server({
+  interfaceForMembership: options.networkInterface,
+  httpServerPort: options.httpPort,
+  httpServerHost: options.httpHost
+})
 
 server.start()
